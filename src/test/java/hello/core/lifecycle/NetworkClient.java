729c48bd704eca29d18998cef0bca330633eff90
DDongYul <comeme0101@naver.com>
2023-01-01 17:28:43 +0900

    빈 생명주기 관리-Bean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -22,7 +22,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     public void call(String message) {
-        System.out.println("call: " + url + "message = " + message);
+        System.out.println("call: " + url + " message = " + message);
     }
 
     //서비스 종료시 호출
@@ -31,15 +31,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존관계 주입이 끝나면 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init(){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
