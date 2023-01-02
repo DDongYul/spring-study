4afae98942e0ee1fd91ea62d2f40f8e64d3724b6
DDongYul <comeme0101@naver.com>
2023-01-02 16:00:42 +0900

    request scope예시코드 -Provider 사용 전 문제 발생상황

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request") //request 스코프 (HTTP request가 들어오고 나갈때까지가 생명주기)
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+    } //[d06b992f...][http://localhost:8080/log-demo] 처럼 출력
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();    //유니크한 숫자 생성
+        System.out.println("[" + uuid + "] request scope bean create:"+ this );
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:"+ this );
+    }
+}
