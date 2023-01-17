2cd38f143324e9069b358b36ede0e878ad4b9f1f
DDongYul <comeme0101@naver.com>
2023-01-17 22:59:20 +0900

    Model추가-서블릿 종속성 제거,뷰 이름 중복제거

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -16,5 +17,18 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request,response);
+    }
+
+    //ModelView를 사용할때 model과 request,resaponse를 모두 넘겨주기 위한 메서드
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //model에서 key,value를 전부 꺼내서 requestAttribute에 담음
+        modelToRequestAttribute(model, request);
+        render(request,response);
+//        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+//        dispatcher.forward(request,response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key,value));
     }
 }
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,62 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+
+        ControllerV3 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        //request에서 모든 파라미터들 받아서 paramMap에 넣어줌
+        Map<String, String> paramMap = createParamMap(request);
+
+        //request 대신 paramMap을 넣어줌 (Model의 역할)
+        ModelView mv = controller.process(paramMap);
+
+        String viewName = mv.getViewName();//논리이름 (new-form , save-result ...)
+        MyView view = viewResolver(viewName);   //논리 이름을 통해 viewPath 만들고 Myview를 만들어 반환
+
+        view.render(mv.getModel(),request,response);
+
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName-> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
