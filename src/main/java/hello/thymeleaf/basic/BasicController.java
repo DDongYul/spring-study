ba279c6664f42278b98b37da186f3d4db149a5dc
DDongYul <comeme0101@naver.com>
2023-02-07 14:52:01 +0900

    연산

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -75,13 +75,19 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
             return "Hello" + data;
         }
     }
-
 
     @Data
     static class User {
