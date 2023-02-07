131c2633ae60c2d4d75d47498bffdae05b83bf70
DDongYul <comeme0101@naver.com>
2023-02-07 15:06:09 +0900

    속성 값 설정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -82,6 +82,13 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "/basic/attribute";
+    }
+
+
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
