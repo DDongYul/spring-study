b35cfb0ab4e37ed1aef0c866fe4c541793a39fc2
DDongYul <comeme0101@naver.com>
2023-02-07 15:22:10 +0900

    반복

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -87,6 +88,11 @@ public class BasicController {
         return "/basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
 
 
     @Component("helloBean")
@@ -94,6 +100,15 @@ public class BasicController {
         public String hello(String data) {
             return "Hello" + data;
         }
+    }
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users",list);
     }
 
     @Data
