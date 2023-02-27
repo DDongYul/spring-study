ad9db2296094a0b5b90d6accae37fb3e3a0d9b0f
DDongYul <comeme0101@naver.com>
2023-02-27 16:10:24 +0900

    FieldError 발생시 입력값 유지

--- spring-mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ spring-mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -45,7 +45,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         //검증 로직
@@ -57,6 +57,43 @@ public class ValidationItemControllerV2 {
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             bindingResult.addError(new FieldError("item","quantity", "수량은 0부터 9,999 사이의 값을 입력하세요."));
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어여 합니다. 현재 값 =  + resultPrice"));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+//            model.addAttribute("errors", bindingResult); // 모델에 안넘겨도 알아서 뷰에 넘어감
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.addError(new FieldError("item","itemName","상품 이름은 필수 입니다.")); //FieldError의 두개의 생성자중 하나, 사용자가 입력했던 값이 지워짐
+            bindingResult.addError(new FieldError("item","itemName", item.getItemName(), false,null,null,"상품 이름은 필수 입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item","price", item.getPrice(),false,null,null,"가격은 1,000 ~ 1,000,000 사이의 값을 입력하세요."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false,null,null,"수량은 0부터 9,999 사이의 값을 입력하세요."));
         }
 
         //특정 필드가 아닌 복합 룰 검증
