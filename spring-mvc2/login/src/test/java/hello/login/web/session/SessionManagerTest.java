e9722c3178a9e024652dc2c92be253e2a5c5f6cf
DDongYul <comeme0101@naver.com>
2023-03-06 00:10:24 +0900

    세션 직접 제작, 테스트 코드

---
+++ spring-mvc2/login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,41 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void SessionTest() {
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        //세션 생성
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies()); //response에있는 값(uuid) 가져옴
+
+        //세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+        
+    }
+
+}
