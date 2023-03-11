fc25af2cd74a84aa819b4c26ed2e0f1c16dd0c1c
DDongYul <comeme0101@naver.com>
2023-03-11 23:19:54 +0900

    주석추가

--- spring-mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ spring-mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -18,6 +18,5 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
-
     }
 }
