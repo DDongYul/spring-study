2a189cb5049722ba206031b6a71d95cef59621cc
DDongYul <comeme0101@naver.com>
2023-03-20 17:42:29 +0900

    언체크 에러를 사용하여 문제 해결

---
+++ spring-DB1/jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -0,0 +1,70 @@
+package hello.jdbc.exception.basic;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UnCheckedAppTest {
+
+    @Test
+    void unchecked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(RuntimeSQLException.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic(){
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+
+    static class NetworkClient {
+        public void call(){
+            throw new RuntimeConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call(){
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException{
+            throw new SQLException("ex");
+        }
+
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+    }
