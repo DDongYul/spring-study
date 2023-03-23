210359b0fb508bd1dc3119bedc4b6f0cca22dda9
DDongYul <comeme0101@naver.com>
2023-03-23 19:55:34 +0900

    구성 및 실행

--- spring-DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 
 //@Import(MemoryConfig.class)
-@Import(JdbcTemplateV1Config.class)	//설정 파일 설정
+@Import(JdbcTemplateV2Config.class)	//설정 파일 설정
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")	//컨트롤러만 컴포넌트 스캔 하겠다는 뜻
 public class ItemServiceApplication {
 
