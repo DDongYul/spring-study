419c82d35e7ea5cb2ca2a5cca11ff2e53d4ff148
DDongYul <comeme0101@naver.com>
2023-04-03 16:36:44 +0900

    엔티티(도메인) 클래스 개발 2차

--- spring-jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ spring-jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){
+    }   //값 클래스는 변경 불가능하게 설계해야한다. 기본 생성자를 protected로 두어 인지하기 쉽게 해주자
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
