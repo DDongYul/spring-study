3c9ad0bc12ba6c26ba256dca1204809451c4c577
DDongYul <comeme0101@naver.com>
2023-04-03 19:56:48 +0900

    회원 리포지토리, 서비스 개발

--- spring-jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ spring-jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -14,20 +14,5 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    @Rollback(false)
-    public void testMember() {
-        Member member = new Member();
-        member.setUsername("memberA");
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
-        Assertions.assertThat(findMember).isEqualTo(member);
-    }
 
 }
