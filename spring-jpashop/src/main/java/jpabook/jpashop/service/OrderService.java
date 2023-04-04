72f48b58e0399cffa89cc1dd7beb3e37a1a884e4
DDongYul <comeme0101@naver.com>
2023-04-04 15:16:00 +0900

    주문 리포지토리, 주문 서비스 -검색기능 미구현-

--- spring-jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ spring-jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,2 +1,62 @@
-package jpabook.jpashop.service;public class OrderService {
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+//        item.removeStock(count); ->createOrderItem에서 해줌
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setStatus(DeliveryStatus.READY);
+        delivery.setAddress(member.getAddress());
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+//        delivery.setOrder(order);
+//        orderItem.setOrder(order); -> order의 cascade 옵션 때문에 order를 persist 하면 얘네는 알아서 처리 됨!!
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    //취소
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+    //검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
 }
