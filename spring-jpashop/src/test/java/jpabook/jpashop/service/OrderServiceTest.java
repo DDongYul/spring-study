061fa2bc8b74f2e48ef8cb6d42d55782bb39c6fb
DDongYul <comeme0101@naver.com>
2023-04-04 16:53:44 +0900

    상품 리포지토리 테스트

--- spring-jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ spring-jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,11 +1,9 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -42,28 +40,36 @@ class OrderServiceTest {
 
         assertThat(getOrder.getStatus()).isEqualTo(OrderStatus.ORDER);
         assertThat(getOrder.getOrderItems().size()).isEqualTo(1);
-        assertThat(book.getStockQuantity()).isEqualTo(5);
-        assertThat(getOrder.getTotalPrice()).isEqualTo(50000);
-
-
+        assertThat(book.getStockQuantity()).isEqualTo(10-orderCount);
+        assertThat(getOrder.getTotalPrice()).isEqualTo(10000*orderCount);
     }
 
     @Test
     public void 주문취소() throws Exception{
         //given
-
+        Member member = createMember();
+        Book book = createBook("책1", 10000, 10);
+        Long orderId = orderService.order(member.getId(), book.getId(), 5);
         //when
-
+        orderService.cancelOrder(orderId);
         //then
+        Order order = orderRepository.findOne(orderId);
+
+        assertThat(order.getStatus()).isEqualTo(OrderStatus.CANCEL);
+        assertThat(book.getStockQuantity()).isEqualTo(10);
     }
 
     @Test
     public void 상품주문_재고수량초과() throws Exception{
         //given
-
+        Member member = createMember();
+        Book book = createBook("책1", 10000, 10);
         //when
-
+        int orderCount = 11;
         //then
+       assertThrows(NotEnoughStockException.class,
+                () -> OrderItem.createOrderItem(book, book.getPrice(), orderCount));
+
     }
 
     private Member createMember() {
