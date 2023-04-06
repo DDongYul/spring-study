dd2eede326cd0b93112cf0fe7973122703e57764
DDongYul <comeme0101@naver.com>
2023-04-07 01:02:14 +0900

    ***중요*** default_batch_fetch_size 설정을 통해 페이징 문제 해결-개수만큼 IN 쿼리 조회하여 쿼리 호출 수 최적화

--- spring-jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ spring-jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -81,4 +81,14 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+
+    }
 }
