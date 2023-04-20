dd9e617b641e40d4b8a65c4d78320a150495d282
DDongYul <comeme0101@naver.com>
2023-04-20 19:52:46 +0900

    ApiOperation 설정 추가

--- spring-DB2/itemservice-db/src/main/java/hello/itemservice/web/ItemController.java
+++ spring-DB2/itemservice-db/src/main/java/hello/itemservice/web/ItemController.java
@@ -4,6 +4,7 @@ import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.service.ItemService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +20,7 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @ApiOperation(value = "전체 아이템 리스트", notes = "전체 아이템 목록 및 정보 출력")
     @GetMapping
     public String items(@ModelAttribute("itemSearch") ItemSearchCond itemSearch, Model model) {
         List<Item> items = itemService.findItems(itemSearch);
@@ -26,6 +28,7 @@ public class ItemController {
         return "items";
     }
 
+    @ApiOperation(value = "아이템 정보", notes = "해당 아이템의 정보 출력")
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemService.findById(itemId).get();
@@ -33,11 +36,13 @@ public class ItemController {
         return "item";
     }
 
+    @ApiOperation(value = "아이템 등록 form", notes = "아이템 등록 form")
     @GetMapping("/add")
     public String addForm() {
         return "addForm";
     }
 
+    @ApiOperation(value = "아이템 등록", notes = "아이템 등록")
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         Item savedItem = itemService.save(item);
@@ -46,6 +51,7 @@ public class ItemController {
         return "redirect:/items/{itemId}";
     }
 
+    @ApiOperation(value = "아이템 수정 form", notes = "아이템 수정 form")
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemService.findById(itemId).get();
@@ -53,6 +59,7 @@ public class ItemController {
         return "editForm";
     }
 
+    @ApiOperation(value = "아이템 수정", notes = "아이템 수정")
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute ItemUpdateDto updateParam) {
         itemService.update(itemId, updateParam);
